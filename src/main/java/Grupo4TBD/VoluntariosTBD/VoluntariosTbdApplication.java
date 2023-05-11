06749265e7caba63e0bbbd988b2ebbf7a97f004a
BenjaminGonzalezH <56660305+BenjaminGonzalezH@users.noreply.github.com>
2023-05-11 18:16:41 -0400

    Inserción de proyecto SpringBoot generado

---
+++ src/main/java/Grupo4TBD/VoluntariosTBD/VoluntariosTbdApplication.java
@@ -0,0 +1,13 @@
+package Grupo4TBD.VoluntariosTBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntariosTbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntariosTbdApplication.class, args);
+	}
+
+}
