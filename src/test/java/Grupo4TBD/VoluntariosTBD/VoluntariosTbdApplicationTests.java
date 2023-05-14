485b7f2939c651e29c2fd50600870fac3f20ffce
BenjaminGonzalezH <56660305+BenjaminGonzalezH@users.noreply.github.com>
2023-05-14 17:20:15 -0400

    Se agrega línea para cargar datos desde archivo SQL.
    La línea esta comentada para que sea opcional su uso.

--- src/test/java/Grupo4TBD/VoluntariosTBD/VoluntariosTbdApplicationTests.java
+++ src/test/java/Grupo4TBD/VoluntariosTBD/VoluntariosTbdApplicationTests.java
@@ -2,8 +2,11 @@ package Grupo4TBD.VoluntariosTBD;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+/*Descomente estas líneas para probrar importación por archivo sql o python*/
+//@Sql({"/import.sql"})
 class VoluntariosTbdApplicationTests {
 
 	@Test
@@ -11,3 +14,4 @@ class VoluntariosTbdApplicationTests {
 	}
 
 }
+
