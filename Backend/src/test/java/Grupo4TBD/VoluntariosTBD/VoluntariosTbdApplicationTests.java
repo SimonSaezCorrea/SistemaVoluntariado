e4382ca40a2900d8876315ea1c549773d0a28a3f
Simon Saez <nayxxaxx789super@gmail.com>
2023-06-02 18:57:31 -0400

    Cambios de orden

---
+++ Backend/src/test/java/Grupo4TBD/VoluntariosTBD/VoluntariosTbdApplicationTests.java
@@ -0,0 +1,14 @@
+package Grupo4TBD.VoluntariosTBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariosTbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
